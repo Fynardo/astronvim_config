dee7e7a826bcc6175edb1d793a73d1948bf30f89
Diego <dfynar@gmail.com>
2023-07-07 20:53:45 +0200

    added catppuccin theme

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,22 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    config = function()
+      require("catppuccin").setup({
+        flavour = "frappe",
+        background = {
+          light="latte",
+          dark="frappe"
+        }
+      })
+    end,
+  },
+  {
+    "neanias/everforest-nvim",
+    name = "everforest-nvim",
+    config = function()
+      require("everforest").setup {}
+    end,
+  },
 }
